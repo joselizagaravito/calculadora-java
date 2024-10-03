c7feeb98da4d71cc0cdc0e954200a3726fb9d390
joselizagaravito <joselizagaravito@gmail.com>
2024-10-03 03:41:13 -0500

    Corregir error de suma

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora {
+    public int sumar(int a, int b) {
+        return Integer.parseInt(String.valueOf(a)) + Integer.parseInt(String.valueOf(b));
+    }    
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
